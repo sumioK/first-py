6f70b70a786919065e28ed1401876d6ca5d47640
Sumio K <tap.miwo@gmail.com>
2022-10-22 21:23:49 +0900

    basic1

---
+++ python_basic1.py
@@ -0,0 +1,68 @@
+# 変数
+a = 123
+print(a)
+b = 123.45
+print(b)
+c = "こんにちはPython!"
+print(c)
+dog_name = "Pochi"
+print(dog_name)
+number321 = 321
+print(number321)
+
+
+# 四則演算
+a = 7
+b = 3
+
+c = a + b 
+print(c)
+d = a - b 
+print(d)
+e = a * b
+print(e)
+f = a / b
+print(f)
+g = a // b
+print(g)
+h = a % b
+print(h)
+
+hello = "hello"
+world ="world"
+print(hello + world)
+
+
+# Bool値と比較演算子
+a = True
+b = False
+print(a)
+print(b)
+
+c = 3
+d = 4
+
+e = c == d
+print(e)
+f = c < d
+print(f)
+g = c > d
+print(g)
+h = c >= d
+print(h)
+i = c <= d
+print(i)
+j = c != d
+print(j)
+
+# 論理演算子
+a = 3
+b = 4
+c = 5
+
+d = a < b and b < c
+print(d)
+e = a<b or b < c
+print(e)
+f = not a < b
+print(f)
